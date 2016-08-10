9f46d6d1955143b7c8a8a82a69c844428bbcfe60
Andrej Kolodjaznij <kol.andrey@gmail.com>
2016-08-10 19:22:39 +0300

    Exercise_4 some changes

--- Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/MyCountryActivity.java
+++ Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/MyCountryActivity.java
@@ -3,12 +3,27 @@ package com.andrey.kol.exercise_4;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyCountryActivity extends ListActivity {
+
+    private ArrayList<String> countriesArray;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_country);
+
+        countriesArray = new ArrayList<>();
+        countriesArray.addAll(Arrays.asList(getResources().getStringArray(R.array.countries)));
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countriesArray);
+
+        setListAdapter(arrayAdapter);
     }
 }
