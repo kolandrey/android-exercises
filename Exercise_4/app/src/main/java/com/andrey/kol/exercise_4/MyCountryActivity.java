48996589c6e458258b1856c1a32272e2b87bd127
Andrej Kolodjaznij <kol.andrey@gmail.com>
2016-08-10 17:22:22 +0300

    Exercise_4 commit

---
+++ Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/MyCountryActivity.java
@@ -0,0 +1,14 @@
+package com.andrey.kol.exercise_4;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCountryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_country);
+    }
+}
