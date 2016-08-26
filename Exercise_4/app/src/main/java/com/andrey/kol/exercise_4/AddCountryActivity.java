e68dc23ba9c54fa9b6f40b78c6d2c50f096dbd3c
Andrey Kolodiazhnyi <feliss@MacBook-Pro-Andrey.local>
2016-08-26 17:14:28 +0300

    xd

--- Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/AddCountryActivity.java
+++ Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/AddCountryActivity.java
@@ -1,13 +1,60 @@
 package com.andrey.kol.exercise_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddCountryActivity extends AppCompatActivity {
+public class AddCountryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String STR = "STR";
+    private EditText editTextYear;
+    private EditText editTextCountry;
+    private Button buttonAddNew;
+    private Button buttonReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_country);
+
+        editTextYear = (EditText) findViewById(R.id.editTextYear);
+        editTextCountry = (EditText) findViewById(R.id.editTextCountry);
+        buttonAddNew = (Button) findViewById(R.id.buttonAddNew);
+        buttonReturn = (Button) findViewById(R.id.buttonReturn);
+
+        buttonAddNew.setOnClickListener(this);
+        buttonReturn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonAddNew:
+                buttonAddNew();
+                break;
+            case R.id.buttonReturn:
+                setResult(RESULT_CANCELED);
+                finish();
+                break;
+        }
+    }
+
+    private void buttonAddNew() {
+        try {
+            String year = editTextYear.getText().toString();
+            String country = editTextCountry.getText().toString();
+            String str = year + " " + country;
+            Intent intentAddNew = new Intent(this, MyCountryActivity.class);
+            intentAddNew.putExtra(STR, str);
+            setResult(RESULT_OK, intentAddNew);
+            finish();
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), R.string.warning, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/MyCountryActivity.java
+++ Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/MyCountryActivity.java
@@ -1,23 +1,34 @@
 package com.andrey.kol.exercise_4;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MyCountryActivity extends ListActivity {
+public class MyCountryActivity extends ListActivity implements View.OnClickListener {
 
+
+    public static final String STR = "STR";
+    public static final int REQUEST_CODE = 1;
     private ArrayList<String> countriesArray;
     private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_country);
 
         countriesArray = new ArrayList<>();
         countriesArray.addAll(Arrays.asList(getResources().getStringArray(R.array.countries)));
@@ -25,5 +36,21 @@ public class MyCountryActivity extends ListActivity {
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countriesArray);
 
         setListAdapter(arrayAdapter);
+
+        Button buttonAddNew = (Button) findViewById(R.id.buttonAddNew);
+        buttonAddNew.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AddCountryActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) return;
+        countriesArray.add(data.getStringExtra(STR));
     }
 }
