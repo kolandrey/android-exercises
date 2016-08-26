e5cda6554ae051a7bdcefe24ee4844355326447c
Andrey Kolodiazhnyi <feliss@MacBook-Pro-Andrey.local>
2016-08-26 16:57:52 +0300

    Exercise_4 complete

---
+++ Exercise_4/app/src/main/java/com/andrey/kol/exercise_4/AddCountryActivity.java
@@ -0,0 +1,13 @@
+package com.andrey.kol.exercise_4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_country);
+    }
+}
